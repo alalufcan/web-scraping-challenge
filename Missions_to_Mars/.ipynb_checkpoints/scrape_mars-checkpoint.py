85f9d1a07fe97e1c1f2d527450f856fe1de6c297
alalufcan <alalufcan@gmail.com>
2020-07-15 12:54:19 -0400

    Final

--- Missions_to_Mars/.ipynb_checkpoints/scrape_mars-checkpoint.py
+++ Missions_to_Mars/.ipynb_checkpoints/scrape_mars-checkpoint.py
@@ -11,14 +11,13 @@ def get_nasa_mars_news(browser):
     soup = BeautifulSoup(html, 'html.parser')
 
     main = soup.find_all('div', class_='list_text')
-    try:
-        news_title = main.find('div', class_='content_title').get_text()
-        news_p = main.find('div',class_= 'article_teaser_body').get_text()
-    except:
-        news_title = ""
-        news_p = ""
+    news_title = main.find('div', class_='content_title').get_text()
+    news_p = main.find('div',class_= 'article_teaser_body').get_text()
 
-    return news_title,news_p
+    return {
+        "news_title": news_title,
+        "news_p": news_p
+    }
 def get_mars_image_url(browser):
     url_img = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
     browser.visit(url_img)
@@ -39,19 +38,16 @@ def get_mars_weather(browser):
     soup = BeautifulSoup(html, 'html.parser')
 
     mars_weather = soup.find_all('div', attrs={'class':'css-901oao r-hkyrab r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-bnwqim r-qvutc0'})
-    try:
-        url = mars_weather.find('span', class_ = 'css-901oao css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0').text
-    except:
-        url = ""
-    return url
+    url = mars_weather.find('span', class_ = 'css-901oao css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0').text
+    return mars_weather
 
 def get_mars_facts(browser):
     url_facts = 'https://space-facts.com/mars/'
     tables = pd.read_html(url_facts)
     df = tables[2]
     df.columns = ['description', 'value']
-    df.set_index("description",inplace=True)
-    return df.to_html(classes="table table-striped")
+    df.to_html('table.html')
+    return df
 
 def get_mars_hemispheres(browser):
     url_image = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
@@ -67,17 +63,15 @@ def get_mars_hemispheres(browser):
         url_images = soup.find('div', class_="collapsible results").find_all('a')[x]['href']
         title = soup.find('div', class_="collapsible results").find_all('h3')[x].text
         full_url = url_full_img + url_images
-        hemisphere = {"title": title,"img_url": full_url}
-        url_list.append(hemisphere)
-        # url_list.append(full_url)
-        # titles.append(title)
-    print(url_list)
-    return url_list
+        url_list.append(full_url)
+        titles.append(title)
+
+    return full_url
 def scrape():
     executable_path = {'executable_path': 'chromedriver.exe'}
     browser = Browser('chrome', **executable_path, headless=True)
     
-    news_title,news_p = get_nasa_mars_news(browser)
+    mars_news = get_nasa_mars_news(browser)
     mars_image_url = get_mars_image_url(browser)
     mars_weather = get_mars_weather(browser)
     mars_facts = get_mars_facts(browser)
@@ -86,11 +80,10 @@ def scrape():
     # browser.quit()
     
     mars_scraped = {
-        "news_title": news_title,
-        "news_p" : news_p,
-        "featured_image_url": mars_image_url,
+        "mars_news": mars_news,
+        "mars_image_url": mars_image_url,
         "mars_weather": mars_weather,
-        "fact_table": mars_facts,
+        "mars_facts": mars_facts,
         "mars_hemispheres": mars_hemispheres
     }
     
